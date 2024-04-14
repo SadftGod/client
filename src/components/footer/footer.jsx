786ead7098252ae11761f9431579ea0992adc659
SadftGod <giber2017artur@gmail.com>
2024-04-14 10:22:20 +0300

    fix en

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -9,32 +9,32 @@ export default function Footer() {
    const languageRed = useSelector((state) => state.language.value);
    const nav = (name) => {
       navigate(name);
-    };
+   };
 
-    const aboutUs = () =>{
-      if(window.location.pathname !== '/main'){
+   const aboutUs = () => {
+      if (window.location.pathname !== '/main') {
          nav('/main')
       }
 
       setTimeout(() => {
          const element = document.querySelector('.faf_con');
-         if(element){
+         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
-         } 
-       }, 500);
+         }
+      }, 500);
    }
 
-    const ContactUs = () =>{
-      if(window.location.pathname !== '/main'){
+   const ContactUs = () => {
+      if (window.location.pathname !== '/main') {
          nav('/main')
       }
 
       setTimeout(() => {
          const element = document.querySelector('.send_form_con');
-         if(element){
+         if (element) {
             element.scrollIntoView({ behavior: 'smooth' });
-         } 
-       }, 500);
+         }
+      }, 500);
    }
 
 
@@ -46,23 +46,23 @@ export default function Footer() {
                   <img className="short_logo" src={process.env.PUBLIC_URL + '/img/short_logo.svg'} alt="" />
                </div>
                <div className="location_con">
-                  <div className="location"><div>{languageRed==="EN"?"Location":"Місцезнаходження"}:</div>
-                     <div>{languageRed==="EN"?"24413, с. Баланівка, обл. Вінниця":""}</div>
+                  <div className="location"><div>{languageRed === "EN" ? "Location" : "Місцезнаходження"}:</div>
+                     <div>{languageRed === "EN" ? "24413, Balanivka v., Vinnytsia r." : "24413, с. Баланівка, обл. Вінниця"}</div>
                   </div>
                   <div className="contact">
-                     <div>{languageRed==="EN"?"Contact":"Контакти"}</div>
+                     <div>{languageRed === "EN" ? "Contact" : "Контакти"}</div>
                      <a className='user_links' href="tel:+380961126340"><div>+380961126340</div></a>
                      <div>dolynan.grainsupplier@gmail.com</div>
                   </div>
                </div>
                <div className="footer_navigator">
-                  <div onClick={()=>{aboutUs()}}  className="footer_navigator_e">{languageRed==="EN"?"About us":"Про нас"}</div>
-                  <div onClick={()=>{nav('/products')}} className="footer_navigator_e">{languageRed==="EN"?"Our products":"Наші продукти"}</div>
-                  <div onClick={()=>{ContactUs()}} className="footer_navigator_e">{languageRed==="EN"?"Our products":"Написати нам"}x</div>
-                  <div onClick={()=>{nav('/account')}}  className="footer_navigator_e">{languageRed==="EN"?"My account":"Мій акаунт"}</div>
+                  <div onClick={() => { aboutUs() }} className="footer_navigator_e">{languageRed === "EN" ? "About us" : "Про нас"}</div>
+                  <div onClick={() => { nav('/products') }} className="footer_navigator_e">{languageRed === "EN" ? "Our products" : "Наші продукти"}</div>
+                  <div onClick={() => { ContactUs() }} className="footer_navigator_e">{languageRed === "EN" ? "Our products" : "Написати нам"}x</div>
+                  <div onClick={() => { nav('/account') }} className="footer_navigator_e">{languageRed === "EN" ? "My account" : "Мій акаунт"}</div>
                </div>
                <div className="footer_contactus">
-                  <div className="footer_contactus_title">{languageRed==="EN"?"Follow us:":"Стежіть за нами:"}</div>
+                  <div className="footer_contactus_title">{languageRed === "EN" ? "Follow us:" : "Стежте за нами:"}</div>
                   <div className="media_net">
                      <img className="links_img" src={process.env.PUBLIC_URL + '/img/links/inst.svg'} alt="" />
                      <img className="links_img" src={process.env.PUBLIC_URL + '/img/links/facebook.svg'} alt="" />
@@ -75,8 +75,8 @@ export default function Footer() {
 
          </div>
          <div className="footer_bottom">
-             <div>© 2024 Dolyna-N. All rights reserved.</div>
-             <div>Cookies Policy</div>
+            <div>© 2024 Dolyna-N. All rights reserved.</div>
+            <div>Cookies Policy</div>
          </div>
       </div>
    )
