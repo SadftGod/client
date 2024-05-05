3e1673e2bc8839fbbfc9ff5c3bead94ec6b41631
SadftGod <giber2017artur@gmail.com>
2024-05-05 12:20:52 +0300

    liil fix

--- src/pages/product_view/productView.jsx
+++ src/pages/product_view/productView.jsx
@@ -4,20 +4,21 @@ import { useParams } from 'react-router-dom';
 import Cart from "../../modal-menu/cart/cart";
 import { useNavigate } from "react-router-dom";
 
-
+import { useSelector } from 'react-redux';
 export default function ProductView(props) {
+   const languageRed = useSelector((state) => state.language.value);
    const { id } = useParams();
    const [quantity, setQuantity] = useState('')
    const [item, setItem] = useState(null)
 
    const [cart, setCart] = useState("hidden")
 
-   
+
    const navigate = useNavigate();
 
    const nav = (name) => {
       navigate(name);
-    };
+   };
 
    const quantity_change = (e) => {
       let filter_val = e.target.value;
@@ -56,7 +57,7 @@ export default function ProductView(props) {
 
    useEffect(() => {
       Promise.resolve(props.data).then(data => {
-          if (Array.isArray(data) && data.length > 0) {
+         if (Array.isArray(data) && data.length > 0) {
             let fitem = data.find(e => {
                if (e.id.toString() === id) {
                   return e
@@ -64,15 +65,15 @@ export default function ProductView(props) {
                   return 0
                }
             })
-   
+
             if (fitem !== 0) {
                setItem(fitem)
             }
-          }
+         }
       }).catch(error => {
-          console.error('Error with array:', error);
+         console.error('Error with array:', error);
       });
-  }, [props.data,id]);
+   }, [props.data, id]);
 
    useEffect(() => {
       window.scrollTo({ top: 0, behavior: 'smooth' });
@@ -81,13 +82,13 @@ export default function ProductView(props) {
    const request_cart = () => {
       if (!item || quantity.length === 0 || quantity === '0' || quantity === '0.') {
          return 1
-      } else { 
+      } else {
          let element = {
-            id : item.id,
-            name:item.name,
-            description: item.description,
+            id: item.id,
+            name: item.name_en,
+            description: item.description_en,
             imageUrl: item.imageUrl,
-            quantity:quantity
+            quantity: quantity
          }
          props.addtocart(element)
          cart_visible()
@@ -100,7 +101,7 @@ export default function ProductView(props) {
       <section className="product-view_con">
          <main className="product-view">
             <div className="view_content">
-               <header className="view-title">{item ? item.name ? item.name : "Loading" : 'Loading'}</header>
+               <header className="view-title">{item ? languageRed === "UA" ? item.name_ua : languageRed === "EN" ? item.name_en : 'Loading' : 'Loading'}</header>
                <main className="view-form">
                   <input id="form_title" name="form_title" value={quantity} onChange={e => quantity_change(e)} type="text" maxLength="10" className="view_form_input" />
                   <label htmlFor="form_title" className="form_title_label">Quantity in kilograms*</label>
@@ -114,7 +115,7 @@ export default function ProductView(props) {
             </div>
          </main>
          {cart !== "hidden" ? <Cart delete_from_cart={props.delete_from_cart} cartData={props.cartData} cart_visible={cart_visible} /> : ''}
-         <div onClick={()=>{nav("/products")}} className="arrow_back_to_cereals">
+         <div onClick={() => { nav("/products") }} className="arrow_back_to_cereals">
             <div className="arrow_back_button"></div>
          </div>
       </section>
